d8297dfa78c18e5cb7d5cd4fe34a71bd08d80bca
Henrique Siconeli Francisco <henrique.siconeli.f@hotmail.com>
2023-10-04 16:44:42 -0400

    update

--- app.py
+++ app.py
@@ -7,12 +7,27 @@ from time import sleep
 driver = webdriver.Chrome()
 driver.get('http://45.188.183.155:8079/transparencia/')
 
+sleep(10)
+arrecadacao_orcamentaria_geral = driver.find_element(By.XPATH, '//li[@id="LnkMenuReceitas"]/ul/li[@id="lnkReceitaOrcamentaria"]/a[@href="#"]')
+
+# Usar o JavaScript quando o elemento estiver coberto por outro elemento
+driver.execute_script("arguments[0].click()", arrecadacao_orcamentaria_geral)
+
+sleep(15)
+# print(li_receitas)
 # Acessar o Iframe:
 # driver.switch_to.frame('frmPaginaAspx')
 
-# tr_receitas = driver.find_element(By.ID, 'gridReceitas_DXDataRow36')
 
-# # Voltar para o contexto padrão
+
+
+# irrf = driver.find_element(By.XPATH, '//tr[@id="gridReceitas_DXDataRow36"]')
+
+# irr_texto = irrf.text
+
+
+
+# Voltar para o contexto padrão
 # driver.switch_to.default_content()
 
 
