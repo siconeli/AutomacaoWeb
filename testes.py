97cc28d6d03227e5a3878c6dab56135fd24f99ff
Henrique Siconeli Francisco <henrique.siconeli.f@hotmail.com>
2023-10-06 16:56:53 -0400

    update

--- testes.py
+++ testes.py
@@ -135,79 +135,80 @@ for mes in meses_calendario:
 
                 if maior_linha_4:
                     maiores.append(maior_linha_4)
+            print(maiores)
             sleep(10)
 
 
-            localiza_ultimo_dia_linha_5 = driver.find_elements(By.XPATH, '//table[@id="datDataFinal_DDD_C_mt"]/tbody/tr[6]/td[@class="dxeCalendarDay_DevEx"]')
-            localiza_ultimo_dia_linha_6 = driver.find_elements(By.XPATH, '//table[@id="datDataFinal_DDD_C_mt"]/tbody/tr[7]/td[@class="dxeCalendarDay_DevEx"]')
-            if localiza_ultimo_dia_linha_6:
+            # localiza_ultimo_dia_linha_5 = driver.find_elements(By.XPATH, '//table[@id="datDataFinal_DDD_C_mt"]/tbody/tr[6]/td[@class="dxeCalendarDay_DevEx"]')
+            # localiza_ultimo_dia_linha_6 = driver.find_elements(By.XPATH, '//table[@id="datDataFinal_DDD_C_mt"]/tbody/tr[7]/td[@class="dxeCalendarDay_DevEx"]')
+            # if localiza_ultimo_dia_linha_6:
                 
                 
 
-            lista_dias_linha_5 = []
-            lista_dias_linha_6 = []
+            # lista_dias_linha_5 = []
+            # lista_dias_linha_6 = []
 
 
-            for dia in localiza_ultimo_dia_linha_5:
-                if dia:
-                    dia_texto = dia.text
-                    dia_inteiro = int(dia_texto)
-                    lista_dias_linha_5.append(dia_inteiro)
+            # for dia in localiza_ultimo_dia_linha_5:
+            #     if dia:
+            #         dia_texto = dia.text
+            #         dia_inteiro = int(dia_texto)
+            #         lista_dias_linha_5.append(dia_inteiro)
 
-            for dia in localiza_ultimo_dia_linha_6:
-                if dia:
-                    dia_texto = dia.text
-                    dia_inteiro = int(dia_texto)
-                    lista_dias_linha_6.append(dia_inteiro)
-                else:
-                    lista_dias_linha_6.append(0)
+            # for dia in localiza_ultimo_dia_linha_6:
+            #     if dia:
+            #         dia_texto = dia.text
+            #         dia_inteiro = int(dia_texto)
+            #         lista_dias_linha_6.append(dia_inteiro)
+            #     else:
+            #         lista_dias_linha_6.append(0)
 
             
-            if len(lista_dias_linha_5) > 0: 
-                maior_linha_5 = max(lista_dias_linha_5)
+            # if len(lista_dias_linha_5) > 0: 
+            #     maior_linha_5 = max(lista_dias_linha_5)
 
-            if len(lista_dias_linha_6) >= 0: 
-                maior_linha_6 = max(lista_dias_linha_6)
+            # if len(lista_dias_linha_6) >= 0: 
+            #     maior_linha_6 = max(lista_dias_linha_6)
 
 
-            if maior_linha_5:
-                maiores.append(maior_linha_5)
+            # if maior_linha_5:
+            #     maiores.append(maior_linha_5)
             
-            if maior_linha_6:
-                maiores.append(maior_linha_6)
-                
+            # if maior_linha_6:
+            #     maiores.append(maior_linha_6)
 
-            ultimo_dia = str(max(maiores))
+
+            # ultimo_dia = str(max(maiores))
             
-            for dia in localiza_ultimo_dia:
-                if dia.text == ultimo_dia:
-                    dia.click()   
-                    print(f'-> Selecionando o último dia do mês: {ultimo_dia}')
+            # for dia in localiza_ultimo_dia:
+            #     if dia.text == ultimo_dia:
+            #         dia.click()   
+            #         print(f'-> Selecionando o último dia do mês: {ultimo_dia}')
 
-        print(f'-> Fim do mês: {mes}')
-        print('\n')
-        sleep(5)
+        # print(f'-> Fim do mês: {mes}')
+        # print('\n')
+        # sleep(5)
 
-        abri_calendario_inicial = driver.find_element(By.XPATH, '//td[@id="datDataInicial_B-1"]/table/tbody/tr/td').click()
-        print('-> Abrindo calendário data')
-        sleep(5)
+        # abri_calendario_inicial = driver.find_element(By.XPATH, '//td[@id="datDataInicial_B-1"]/table/tbody/tr/td').click()
+        # print('-> Abrindo calendário data')
+        # sleep(5)
 
-        # Pega a data atual, desconta um mês para pegar o mês anterior ao atual, converte o número do mês para o mês por extenso, e monte a string para ser utilizada na condição
-        data_atual = datetime.datetime.now()
-        ano_atual = data_atual.year
-        mes_anterior = data_atual.month -1
-        mes_anterior = str(data_atual.month)
-        mes_anterior_extenso = meses_conversao.get(mes_anterior)
-        mes_anterior_tratado = f'{mes_anterior_extenso} de {ano_atual}'
+        # # Pega a data atual, desconta um mês para pegar o mês anterior ao atual, converte o número do mês para o mês por extenso, e monte a string para ser utilizada na condição
+        # data_atual = datetime.datetime.now()
+        # ano_atual = data_atual.year
+        # mes_anterior = data_atual.month -1
+        # mes_anterior = str(data_atual.month)
+        # mes_anterior_extenso = meses_conversao.get(mes_anterior)
+        # mes_anterior_tratado = f'{mes_anterior_extenso} de {ano_atual}'
 
-        if mes_inicial == mes_anterior_tratado:
-            break
+        # if mes_inicial == mes_anterior_tratado:
+        #     break
 
-        sleep(5)
+        # sleep(5)
 
 
  
-sleep(150)
+# sleep(150)
 
 
 
